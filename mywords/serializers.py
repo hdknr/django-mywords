8bf6914fc137ed250953896b217314d680cbeb1e
hdknr <gmail@hdknr.com>
2017-12-15 14:36:52 +0900

    get_or_create functionality

--- mywords/serializers.py
+++ mywords/serializers.py
@@ -1,4 +1,3 @@
-# encoding: utf-8
 from rest_framework import serializers
 from . import models
 
@@ -8,6 +7,20 @@ class WordSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Word
         fields = '__all__'
+
+    def is_valid(self, *args, **kwargs):
+        res = super(WordSerializer, self).is_valid(*args, **kwargs)
+        if not res:
+            self._existing_instance = self.Meta.model.objects.filter(
+                    text=self.initial_data.get('text', '')).first()
+            res = self._existing_instance and True or False
+        return res
+
+    def save(self, **kwargs):
+        if hasattr(self, '_existing_instance'):
+            self.instance = self._existing_instance
+            return self.instance
+        return super(WordSerializer, self).save(**kwargs)
 
 
 class LinkSerializer(serializers.ModelSerializer):
