12264f7781d67cdd03b434b15d8b2a31ccc58041
hdknr <gmail@hdknr.com>
2017-12-15 14:37:06 +0900

    api

--- mywords/api.py
+++ mywords/api.py
@@ -1,14 +1,12 @@
 # coding: utf-8
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from . import views
+from . import viewsets
 
 router = DefaultRouter()
-router.register(r'words', views.WordViewSet, base_name='word')
-router.register(r'links', views.LinkViewSet, base_name='link')
+router.register(r'word', viewsets.WordViewSet, base_name='word')
+router.register(r'link', viewsets.LinkViewSet, base_name='link')
 
 urlpatterns = [
-    url(r'^api/',
-        include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls)),
 ]
